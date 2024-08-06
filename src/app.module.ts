c71b659e47c9cdebcc75a30941506e7d79f2cbbb
Galez <89478647+Thapakornd@users.noreply.github.com>
2024-08-06 12:42:13 +0700

    [Service]feat: create user schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './models/user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { env } from './config/app.config';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, MongooseModule.forRoot(env.MONGO_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import mongoose from "mongoose";
+
+@Schema()
+export class User {
+
+    @Prop({ required: true, type: mongoose.Schema.Types.String })
+    username: string;
+    
+    @Prop({ required: true, type: mongoose.Schema.Types.String })
+    email: string;
+
+    @Prop({ required: true, type: mongoose.Schema.Types.String })
+    password: string;
+
+    @Prop({ required: true, type: mongoose.Schema.Types.String })
+    firstName: string;
+
+    @Prop({ required: true, type: mongoose.Schema.Types.String })
+    lastName: string;
+
+    @Prop({ default: 0 , type: mongoose.Schema.Types.String })
+    commissionMoney: number;
+
+    @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }] })
+    commissionUser: User[];
+
+    @Prop({ type: mongoose.Schema.Types.String })
+    code: string;
+}
